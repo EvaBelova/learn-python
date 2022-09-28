4ad0ba61d52fe12403155b35988f7e21d9ed702f
Евгения <evgenialutko554@gmail.com>
2022-09-28 14:40:26 +0400

    Семинар 5

---
+++ урок 5/e3.py
@@ -0,0 +1,65 @@
+#Создайте программу для игры в ""Крестики-нолики"".
+
+print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
+print ('''Правила игры.
+       Игроки по очереди ставят на свободные клетки поля 3×3 знаки
+       (один всегда крестики, другой всегда нолики).
+       Первый, выстроивший в ряд 3 своих фигуры по вертикали,
+       горизонтали или диагонали, выигрывает.
+       Первый ход делает игрок, ставящий крестики.''')
+
+board = list(range(1,10))
+
+def draw_board(board):
+   for i in range(3):
+      print(board[0+i*3], "|", board[1+i*3], "|", board[2+i*3])
+      print("-" * 9)
+
+def take_input(player_token):
+   valid = False
+   while not valid:
+      player_answer = input("Куда поставим " + player_token+"? ")
+      try:
+         player_answer = int(player_answer)
+      except:
+         print("Некорректный ввод. Вы уверены, что ввели число?")
+         continue
+      if player_answer >= 1 and player_answer <= 9:
+         if(str(board[player_answer-1]) not in "XO"):
+            board[player_answer-1] = player_token
+            valid = True
+         else:
+            print("Эта клетка уже занята!")
+      else:
+        print("Некорректный ввод. Введите число от 1 до 9.")
+
+def check_win(board):
+   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+   for each in win_coord:
+       if board[each[0]] == board[each[1]] == board[each[2]]:
+          return board[each[0]]
+   return False
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+           take_input("X")
+        else:
+           take_input("O")
+        counter += 1
+        if counter > 4:
+           tmp = check_win(board)
+           if tmp:
+              print(tmp, "выиграл!")
+              win = True
+              break
+        if counter == 9:
+            print("Ничья!")
+            break
+    draw_board(board)
+main(board)
+
+input("Нажмите Enter для выхода!")
